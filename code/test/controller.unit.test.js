3e122be0d020bcacf4ed82208ee9dc2f24f3e256
s315221 <s315221@studenti.polito.it>
2023-05-30 12:54:33 +0200

    unit test getcategories success

--- code/test/controller.unit.test.js
+++ code/test/controller.unit.test.js
@@ -2,7 +2,7 @@ import request from 'supertest';
 import { app } from '../app';
 import { categories, transactions } from '../models/model';
 import { verifyAuth } from '../controllers/utils';
-import { createCategory, deleteCategory, updateCategory } from '../controllers/controller';
+import { createCategory, deleteCategory, getCategories, updateCategory } from '../controllers/controller';
 
 jest.mock('../models/model');
 jest.mock('../controllers/utils');
@@ -40,6 +40,13 @@ describe(
                     locals: {
                     }
                 }
+                const mockResStatus = 200
+                const mockResData = {
+                    data: {
+                        type: "testType",
+                        color: "testColor"
+                    }
+                }
 
                 verifyAuth.mockImplementation(() => { return { flag: true, cause: 'Authorized' } })
                 jest.spyOn(categories, 'findOne').mockResolvedValue(null);
@@ -50,8 +57,8 @@ describe(
                 expect(verifyAuth).toHaveBeenCalledWith(mockReq, mockRes, { authType: 'Admin' });
                 expect(categories.prototype.save).toHaveBeenCalledWith();
                 //expect(categories.find).toHaveBeenCalledWith({ type: "testType" });
-                expect(mockRes.status).toHaveBeenCalledWith(200);
-                expect(mockRes.json).toHaveBeenCalledWith({ data: { type: "testType", color: "testColor" }, message: mockRes.locals.message });
+                expect(mockRes.status).toHaveBeenCalledWith(mockResStatus);
+                expect(mockRes.json).toHaveBeenCalledWith(mockResData);
             }
         );
     })
@@ -83,6 +90,7 @@ describe("updateCategory", () => {
                 color: 'currentColor',
                 save: jest.fn()
             }
+            const mockResStatus = 200
             const mockResData = {
                 data: {
                     message: "Category edited successfully",
@@ -107,7 +115,7 @@ describe("updateCategory", () => {
             expect(categories.findOne).toHaveBeenCalledWith({ type: mockReq.body.type });
             expect(mockCurrentCategory.save).toHaveBeenCalled()
             expect(transactions.updateMany).toHaveBeenCalledWith({ type: mockReq.params.type }, { type: mockReq.body.type });
-            expect(mockRes.status).toHaveBeenCalledWith(200);
+            expect(mockRes.status).toHaveBeenCalledWith(mockResStatus);
             expect(mockRes.json).toHaveBeenCalledWith(mockResData);
         }
     );
@@ -138,6 +146,7 @@ describe("updateCategory", () => {
                 color: 'currentColor',
                 save: jest.fn()
             }
+            const mockResStatus = 200
             const mockResData = {
                 data: {
                     message: "Category edited successfully",
@@ -157,7 +166,7 @@ describe("updateCategory", () => {
             expect(categories.findOne).toHaveBeenCalledWith({ type: mockReq.params.type });
             expect(mockCurrentCategory.save).toHaveBeenCalled()
             expect(transactions.updateMany).toHaveBeenCalledWith({ type: mockReq.params.type }, { type: mockReq.body.type });
-            expect(mockRes.status).toHaveBeenCalledWith(200);
+            expect(mockRes.status).toHaveBeenCalledWith(mockResStatus);
             expect(mockRes.json).toHaveBeenCalledWith(mockResData);
         }
     );
@@ -179,17 +188,21 @@ describe("deleteCategory", () => {
             }
         }
         const mockCategories = [
-            { _id: 0, type: "A" },
-            { _id: 1, type: "B" },
-            { _id: 2, type: "C" },
-            { _id: 3, type: "D" },
-            { _id: 4, type: "E" },
+            { _id: 0, type: "A", color: "A" },
+            { _id: 1, type: "B", color: "B" },
+            { _id: 2, type: "C", color: "C" },
+            { _id: 3, type: "D", color: "D" },
+            { _id: 4, type: "E", color: "E" },
         ]
+
         const mockTypesToDelete = ["C", "B", "D", "E"];
         const mockModifiedCount = 15;
+        const mockResStatus = 200
         const mockResData = {
-            message: "Categories deleted",
-            count: mockModifiedCount
+            data: {
+                message: "Categories deleted",
+                count: mockModifiedCount
+            }
         }
 
         verifyAuth.mockImplementation(() => { return { flag: true, cause: 'Authorized' } });
@@ -202,8 +215,8 @@ describe("deleteCategory", () => {
         expect(verifyAuth).toHaveBeenCalledWith(mockReq, mockRes, { authType: 'Admin' });
         expect(categories.deleteMany).toHaveBeenCalledWith({ type: { $in: mockTypesToDelete } });
         expect(transactions.updateMany).toHaveBeenCalledWith({ type: { $in: mockTypesToDelete } }, { type: mockCategories[0].type });
-        expect(mockRes.status).toHaveBeenCalledWith(200);
-        expect(mockRes.json).toHaveBeenCalledWith({ data: mockResData });
+        expect(mockRes.status).toHaveBeenCalledWith(mockResStatus);
+        expect(mockRes.json).toHaveBeenCalledWith(mockResData);
 
     });
 
@@ -222,17 +235,20 @@ describe("deleteCategory", () => {
             }
         }
         const mockCategories = [
-            { _id: 0, type: "A" },
-            { _id: 1, type: "B" },
-            { _id: 2, type: "C" },
-            { _id: 3, type: "D" },
-            { _id: 4, type: "E" },
+            { _id: 0, type: "A", color: "A" },
+            { _id: 1, type: "B", color: "B" },
+            { _id: 2, type: "C", color: "C" },
+            { _id: 3, type: "D", color: "D" },
+            { _id: 4, type: "E", color: "E" },
         ]
         const mockTypesToDelete = ["C", "B", "A", "D"];
         const mockModifiedCount = 15;
+        const mockResStatus = 200
         const mockResData = {
-            message: "Categories deleted",
-            count: mockModifiedCount
+            data: {
+                message: "Categories deleted",
+                count: mockModifiedCount
+            }
         }
 
         verifyAuth.mockImplementation(() => { return { flag: true, cause: 'Authorized' } });
@@ -245,15 +261,81 @@ describe("deleteCategory", () => {
         expect(verifyAuth).toHaveBeenCalledWith(mockReq, mockRes, { authType: 'Admin' });
         expect(categories.deleteMany).toHaveBeenCalledWith({ type: { $in: mockTypesToDelete } });
         expect(transactions.updateMany).toHaveBeenCalledWith({ type: { $in: mockTypesToDelete } }, { type: "E" });
-        expect(mockRes.status).toHaveBeenCalledWith(200);
-        expect(mockRes.json).toHaveBeenCalledWith({ data: mockResData });
+        expect(mockRes.status).toHaveBeenCalledWith(mockResStatus);
+        expect(mockRes.json).toHaveBeenCalledWith(mockResData);
 
     });
 })
 
 describe("getCategories", () => {
-    test('Dummy test, change it', () => {
-        expect(true).toBe(true);
+    test('should retreive all categories', async () => {
+        const mockReq = {
+            params: {
+            },
+            body: {
+            }
+        }
+        const mockRes = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+            locals: {
+            }
+        }
+        const mockCategories = [
+            { _id: 0, type: "A", color: "A" },
+            { _id: 1, type: "B", color: "B" },
+            { _id: 2, type: "C", color: "C" },
+            { _id: 3, type: "D", color: "D" },
+            { _id: 4, type: "E", color: "E" },
+        ]
+        const mockResStatus = 200
+        const mockResData = {
+            data: [
+                { type: "A", color: "A" },
+                { type: "B", color: "B" },
+                { type: "C", color: "C" },
+                { type: "D", color: "D" },
+                { type: "E", color: "E" },
+            ]
+        }
+
+        verifyAuth.mockImplementation(() => { return { flag: true, cause: 'Authorized' } });
+        categories.find.mockResolvedValue(mockCategories);
+
+        await getCategories(mockReq, mockRes);
+
+        expect(mockRes.status).toHaveBeenCalledWith(mockResStatus);
+        expect(mockRes.json).toHaveBeenCalledWith(mockResData);
+
+    });
+
+    test('should retreive no category', async () => {
+        const mockReq = {
+            params: {
+            },
+            body: {
+            }
+        }
+        const mockRes = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+            locals: {
+            }
+        }
+        const mockCategories = []
+        const mockResStatus = 200
+        const mockResData = {
+            data: []
+        }
+
+        verifyAuth.mockImplementation(() => { return { flag: true, cause: 'Authorized' } });
+        categories.find.mockResolvedValue(mockCategories);
+
+        await getCategories(mockReq, mockRes);
+
+        expect(mockRes.status).toHaveBeenCalledWith(mockResStatus);
+        expect(mockRes.json).toHaveBeenCalledWith(mockResData);
+
     });
 })
 
