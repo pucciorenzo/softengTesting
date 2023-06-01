f23018bffd47b70aab052f94fd6f184b5a20ee72
s315221 <s315221@studenti.polito.it>
2023-06-01 13:33:19 +0200

    move validate functions to extrautils.js

--- code/controllers/controller.js
+++ code/controllers/controller.js
@@ -2,116 +2,14 @@ import { categories, transactions } from "../models/model.js";
 import { Group, User } from "../models/User.js";
 import { handleDateFilterParams, handleAmountFilterParams, verifyAuth } from "./utils.js";
 
-import validator from 'validator';
-
+import { createAttribute, validateAttribute, validateAttributes, resError, resData } from "./extraUtils.js";
 
 /**
  * Error response:
  * res.status(errorCode).json({ error: "Error message" });
  */
 
-const createAttribute = (v, t) => { return { value: v, type: t } }
-const validationFail = (c) => { return { flag: false, cause: c } }
-const validationPass = () => { return { flag: true, cause: 'valid' } }
-const validateAttribute = (attribute) => {
-    try {
 
-        const { value, type } = attribute;
-
-        //incomlete/missing attribute
-        if (typeof value == "undefined" || value == null) return validationFail("incomplete attribute");
-
-        switch (type) {
-
-            case 'string':
-                {
-                    //not a string
-                    if (typeof value != 'string') return validationFail("not string");
-
-                    //emty string or all whitespace
-                    if (value.trim().length == 0) return validationFail("empty string");
-                }
-                break;
-
-            case 'stringArray':
-                {
-                    const array = value;
-
-                    //not an array
-                    if (!Array.isArray(array)) return validationFail("not array");
-
-                    //no element
-                    if (array.length == 0) return validationFail("empty array");
-
-                    //repeating elements or invalid elements//
-                    const exists = {};
-                    for (const value of array) {
-
-                        //not valid string element
-                        let validation = validateAttribute(createAttribute(value, 'string'));
-                        if (!validation.flag) return validationFail("at least one empty string");
-
-                        //repeating element
-                        if (exists[value]) return validationFail("at least one repeating element");
-                        exists[value] = true;
-                    }
-                }
-                break;
-
-            case 'amount':
-            case 'number':
-            case 'float': {
-                //not a number after parsing
-                if (isNaN(parseFloat(value))) return validationFail("cannot parse as floating value");
-            }
-                break;
-
-            case 'email':
-                {
-                    //incorrect format or invalid characters
-                    if (!validator.isEmail(value)) return validationFail("not an email");
-                }
-                break;
-
-            case 'date':
-                {
-                    //not YYYY-MM-DD
-                    const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
-                    if (!dateRegex.test(value)) validationFail("invalid date format");
-                }
-                break;
-
-            default:
-                //not known type
-                return validationFail("unknown type");
-        }
-
-        //all check passed
-        return validationPass();
-
-    } catch (error) {
-        return validationFail(error.message);
-    }
-
-}
-
-const validateAttributes = (attributes) => {
-    try {
-        for (const a of attributes) {
-            let validation = validateAttribute(a);
-            if (!validation.flag) return validation;
-        }
-        return validationPass();
-
-    } catch (error) {
-        console.log(error);
-        return { flag: false, cause: error.message };
-    }
-}
-
-
-const resError = (res, code, msg) => res.status(code).json({ error: msg });
-const resData = (res, data) => res.status(200).json({ data: data, refreshedTokenMessage: res.locals.refreshedTokenMessage });
 
 /**
 * createCategory
